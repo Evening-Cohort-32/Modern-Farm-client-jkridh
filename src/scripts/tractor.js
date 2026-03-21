88e3035966e21921c9c3dfa6f9eb7433889256ef
Lake Carter <lakecarter8@gmail.com>
2026-03-21 12:48:05 -0500

    started logic for plantSeeds(). Pushing for testing

---
+++ src/scripts/tractor.js
@@ -0,0 +1,33 @@
+import {createSoybean} from "./seeds/soybean.js"
+import {createCorn} from "./seeds/corn.js"
+import {createSunflower} from "./seeds/Sunflower.js"
+import {createAsparagus} from "./seeds/Asparagus.js"
+import {createWheat} from "./seeds/Wheat.js"
+import {createPotato} from "./seeds/Potato.js"
+import{addPlant} from "./field.js"
+
+export const plantSeeds = (yearlyPlan) => {
+    for (const row of yearlyPlan){
+        for(const plant of row) {
+            if (plant == Soybean){
+                addPlant(createSoybean())
+            }
+            else if(plant == Corn){
+                addPlant(createCorn())
+            }
+            else if(plant == Sunflower){
+                addPlant(createSunflower())
+            }
+            else if(plant == Asparagus){
+                addPlant(createAsparagus())
+            }
+            else if(plant == Wheat){
+                addPlant(createWheat())
+            }
+            else if(plant == Potato){
+                addPlant(createPotato())
+            }
+        }
+    }
+}
+
