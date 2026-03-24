abe66d65fcaa82f9b2acf35fb13a3c9ccefd8b1c
Jason LeGlue <jasonleglue@gmail.com>
2026-03-24 18:03:20 -0500

    Completed project requirements, passed tests

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -2,12 +2,17 @@ let seedArray = [];
 
 export const harvestPlants = (plants) => {
   for (const plant of plants) {
+    let idNumber = 1;
     if (plant.type == "Corn") {
       for (let i = 0; i < plant.output / 2; i++) {
+        plant.id = idNumber;
+        idNumber++;
         seedArray.push(plant);
       }
     } else {
       for (let i = 0; i < plant.output; i++) {
+        plant.id = idNumber;
+        idNumber++;
         seedArray.push(plant);
       }
     }
